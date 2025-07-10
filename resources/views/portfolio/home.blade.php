fc73dd0b966b7fab32595ad572b3d13d6b6b8f52
Dan33760 <dansivyolo@gmail.com>
2025-07-11 00:03:53 +0200

    Add menu small screen

--- resources/views/portfolio/home.blade.php
+++ resources/views/portfolio/home.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,10 +8,110 @@
     <title>Document</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-    <main class="bg-blue-300">
 
-        <h1 class="text text-blue-300">Dan</h1>
+<body class="bg-blue-300 h-auto lg:h-screen w-full flex items-center justify-center font-monda text-white-100">
+    <main class="relative flex w-full lg:w-[95%] lg:h-[95%] border border-amber-50 ">
+        <aside class="relative hidden lg:block w-[25%] z-50 my-5">
+            <div
+                class="flex flex-col justify-between px-8 py-5 w-full h-full bg-blue-200/40 backdrop-blur-[0.5px] rounded-[22px] ">
+                <div class="text-5xl font-extrabold">D.</div>
+                <div class="text-center">
+                    <ul class="flex flex-col gap-5 text-2xl">
+                        <li class="hover:text-green-100"><a href="#">Acceuil</a></li>
+                        <li class="hover:text-green-100"><a href="#">Services</a></li>
+                        <li class="hover:text-green-100"><a href="#">A propos</a></li>
+                        <li class="hover:text-green-100"><a href="#">Competences</a></li>
+                        <li class="hover:text-green-100"><a href="#">Projets</a></li>
+                        <li class="hover:text-green-100"><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <a href="#"
+                        class="flex justify-center gap-5 items-center text-gray-100 border-2 w-full h-[52px] rounded-[11px] border-blue-100">
+                        <svg width="32" height="31" viewBox="0 0 32 31" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M4 21.3125V24.2187C4 24.9895 4.31607 25.7288 4.87868 26.2738C5.44129 26.8188 6.20435 27.125 7 27.125H25C25.7957 27.125 26.5587 26.8188 27.1213 26.2738C27.6839 25.7288 28 24.9895 28 24.2187V21.3125M22 15.5L16 21.3125M16 21.3125L10 15.5M16 21.3125V3.875"
+                                stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        Télécharger CV
+                    </a>
+                </div>
+            </div>
+        </aside>
+        <aside class="fixed left-[2.5%] right-[2.5%] bottom-1 flex lg:hidden w-[95%] h-16 z-50 my-5 text-gray-100">
+            <div
+                class="flex justify-between px-5 py-5 w-full h-full bg-blue-200/40 backdrop-blur-[0.5px] rounded-[22px] ">
+                <a href="#" class="w-auto flex flex-col justify-center items-center">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                        </svg>
+                    </div>
+                    <p class="text-[10px]">Acceuil</p>
+                </a>
+                <a href="#" class="w-auto flex flex-col justify-center items-center">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+                    </svg>
+                    </div>
+                    <p class="text-[10px]">Services</p>
+                </a>
+                <a href="#" class="w-auto flex flex-col justify-center items-center">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                    </svg>
+                    </div>
+                    <p class="text-[10px]">A propos</p>
+                </a>
+                <a href="#" class="w-auto flex flex-col justify-center items-center">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+                    </svg>
+                    </div>
+                    <p class="text-[10px]">Competences</p>
+                </a>
+                <a href="#" class="w-auto flex flex-col justify-center items-center">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
+                    </svg>
+                    </div>
+                    <p class="text-[10px]">Projets</p>
+                </a>
+                <a href="#" class="w-auto flex flex-col justify-center items-center">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                    </svg>
+                    </div>
+                    <p class="text-[10px]">Contact</p>
+                </a>
+            </div>
+        </aside>
+        <div class="relative flex-grow h-full border border-amber-600">
+            Hello
+            <img src="{{ asset('images/bg-img-2.webp') }}" class="absolute left-3 lg:left-[30%] bottom-0 z-10"
+                alt="eclipse 1">
+        </div>
+        <img src="{{ asset('images/eclipse-1.svg') }}" class="absolute left-1 lg:left-[10%] -top-[3%] z-10"
+            alt="eclipse 1">
     </main>
 </body>
+
 </html>
