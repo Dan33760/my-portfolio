2e1a7d00f750ba1443f330ffc978e7a42d8008dc
Dan33760 <dansivyolo@gmail.com>
2025-07-18 20:54:31 +0200

    Test of navigation after adding turbo

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,8 @@ Route::get('/page', function () {
     return view('welcome');
 });
 
-Route::get('/', [PortfolioController::class, 'home']);
-Route::get('/services', [PortfolioController::class, 'services']);
+// Route::get('/', [PortfolioController::class, 'home']);
+// Route::get('/services', [PortfolioController::class, 'services']);
+
+Route::view('/', 'portfolio.home');
+Route::view('/services', 'portfolio.services');
